a22952bcadf9df1636f44ac2df2f9583fe23eb0c
sefortescue <yzj8b4ztkh9cmhxswqtz@tutanota.com>
2022-07-15 17:52:41 -0600

    finish getopt stuff

--- main.cc
+++ main.cc
@@ -1,38 +1,72 @@
 #include <iostream>
+#include <string>
 #include <unistd.h>
 #include <getopt.h>
-#include <string>
 
 using std::cout;
 using std::cerr;
 using std::string;
 
+// global for error messages
 string program_name;
 
 int main(int argc, char *argv[]) {
-	
+
+	// store program name for error messages	
 	program_name = argv[0];
+
+	// option flags
+	bool sFlag = false;
+	bool bFlag = false;
+	bool iFlag = false;
+
+	// option args
+	string sArg = "";
+	string bArg = "";
+	string iArg = "";
 	
-	//options
-	
-	int opt;	
-	char opOut;
+	// varible to store option characters
+	int opt;
+
+	// print my own error messages	
 	opterr = 0;
-	
-	while ((opt = getopt(argc,argv,"+:r:fsc:v")) != -1) {
+
+	// while there are still remaining legal options set a flag and store any arguments
+	while ((opt = getopt(argc,argv,"+:s:b:i:")) != -1) {
 		switch (opt) {
 		case '?':
-			opOut = optopt;
-			cerr << program_name << ": unknown option given: -" << opOut << "\n";
+			cerr << program_name << ": unknown option given: -" << optopt << "\n";
 			return 1;
-			break;
 		case ':':
-			opOut = optopt;
-			cerr << program_name << ": missing option argument at: -" << opOut << "\n";
-			return 2;
+			cerr << program_name << ": missing option argument at: -" << optopt << "\n";
+			return 1;
+		case 's':
+			if (sFlag) {
+				cerr << program_name << ": -s given more than once, arg use is ambiguous." << "\n";
+				return 1;
+			}
+			sFlag = true;
+			sArg = optarg;
+			break;
+		case 'b':
+			if (bFlag) {
+				cerr << program_name << ": -b given more than once, arg use is ambiguous." << "\n";
+				return 1;
+			}
+			bFlag = true;
+			bArg = optarg;
+			break;
+		case 'i':
+			if (iFlag) {
+				cerr << program_name << ": -i given more than once, arg use is ambiguous." << "\n";
+				return 1;
+			}
+			iFlag = true;
+			iArg = optarg;
 			break;
 		}
 	}
-	
+
+
 	return 0;
 }
